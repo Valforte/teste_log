6cd7dc3707a611ec85132df55171f95e9df47f4d
Valforte <Valforte@users.noreply.github.com>
2024-06-27 16:57:12 -0300

    enviando codigo

--- log-api.py
+++ log-api.py
@@ -1,45 +1,20 @@
-import requests
-import json
+import pandas as pd
+from pandasai import SmartDataframe
+from pandasai.llm import OpenAI
 
-url = "https://www.chatcsv.co/api/v1/chat"
-token = "sk_3ZJhx6vPvtYAu15rLFHGdfFe"
+openai_api_key  = 'OPEN_AI_API_KEY'
+llm = OpenAI(api_token=openai_api_key)
 
-messages = [
-    {
-        "role": "system",
-        "content": f'''Você é um assistente virtual de uma empresa de comércio exterior. 
-            Você pode apenas perguntas que se refiram aos dados enviados no arquivo "titanic.csv". 
-            Qualquer pergunta fora deste contexto você deve se desculpar e não responder.'''
-     },
-]
+df = pd.read_csv("./moat_202404.csv")
 
-payload = {
-    "model": "gpt-3.5-turbo-16k-0613",
-    "messages": messages,
-    "files": [
-        "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
-    ]
-}
-
-headers = {
-    'accept': 'text/event-stream',
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {token}'
-}
+chat_df = SmartDataframe(df, config={"llm": llm})
 
 def ask_assistant(question):
-    messages.append({ "role": "user", "content": question })
-    result = requests.post(url, headers=headers, data=json.dumps(payload), stream=True)
-    response = ''
+    return chat_df.chat(question)
 
-    for line in result.iter_lines():
-        if line:
-            decoded_line = line.decode('utf-8')
-            response += decoded_line
-    return response
-    
 print("Para sair a qualquer momento aperte CTRL + C")
 
 while(True):
     question = input("Pergunta: ")
-    print("Resposta: " + ask_assistant(question))
+    print("Resposta: ")
+    print(ask_assistant(question))
